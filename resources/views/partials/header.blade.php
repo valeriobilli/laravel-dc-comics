7834df4a0eb643e005376a889f6ccf1a42c9f0d9
Valerio Billi <valerio.billi18@gmail.com>
2023-07-04 15:59:28 +0200

    add logo header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,4 @@
 <header class="p-3">
-    <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
+    <img class="logo" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="DC Logo">
     <span>Some links: </span>
-    @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
-    @endforeach
 </header>
