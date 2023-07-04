500bc74d928661c6a7352b14340555248f6ba3cb
Valerio Billi <valerio.billi18@gmail.com>
2023-07-04 16:23:51 +0200

    migration

--- database/migrations/2023_07_04_140257_create_comics_table.php
+++ database/migrations/2023_07_04_140257_create_comics_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+
+            $table->tinytext("title");
+            $table->text("description");
+            $table->string("thumb");
+            $table->smallint("price");
+            $table->string("series", 50);
+            $table->date("sale_date");
+            $table->string("type", 50);
+            $table->text("artists");
+            $table->text("writers");
+
             $table->timestamps();
         });
     }
