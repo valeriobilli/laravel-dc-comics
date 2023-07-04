4a53fafe2ba1e728a66ca8230ada1d851f946151
Valerio Billi <valerio.billi18@gmail.com>
2023-07-04 16:53:13 +0200

    add model Comic

--- database/seeders/ComicTableSeeder.php
+++ database/seeders/ComicTableSeeder.php
@@ -14,6 +14,20 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config("comics");
+
+        foreach ($comics as $comic) {
+            $newComic = new Comic();
+            $newComic->title = $comic["title"];
+            $newComic->description = $comic["description"];
+            $newComic->thumb = $comic["thumb"];
+            $newComic->price = $comic["price"];
+            $newComic->series = $comic["series"];
+            $newComic->sale_date = $comic["sale_date"];
+            $newComic->type = $comic["type"];
+            $newComic->artists = $comic["artists"];
+            $newComic->writers = $comic["writers"];
+            $newComic->save();
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(ComicTableSeeder::class);
     }
 }
