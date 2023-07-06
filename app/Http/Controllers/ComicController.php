36052f2062e3d879f29ce1236f5d8faf535cbd0e
Valerio Billi <valerio.billi18@gmail.com>
2023-07-06 16:41:14 +0200

    storage create data

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -37,7 +37,18 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newComic = new Comic;
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->artists = $data['artists'];
+        $newComic->writers = $data['writers'];
     }
 
     /**
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,31 +5,43 @@
     <div class="container my-3">
         <h1>Create</h1>
         <div class="row g-4">
-            <div class="col d-flex flex-wrap">
+            <div class="col">
                 <form action="{{ route('comics.store')}}" method="post">
-                    @csrf
-
+                    @csrf {{-- va inserito in ogni form per questioni di sicurezza  --}}
+                     
                     <label for="title">Titolo</label>
-                    <input type="text" name="title" id="title">
+                    <input class="form-control mb-3" type="text" name="title" id="title">
 
                     <label for="description">Descrizione</label>
-                    <input type="textarea" name="description" id="description">
+                    <input class="form-control mb-3" type="textarea" name="description" id="description">
 
                     <label for="thumb">Copertina</label>
-                    <input type="text" name="thumb" id="thumb">
+                    <input class="form-control mb-3" type="text" name="thumb" id="thumb">
 
                     <label for="price">Prezzo</label>
-                    <input type="text" name="price" id="price">
+                    <input class="form-control mb-3" type="text" name="price" id="price">
+
+                    <label for="series">Serie</label>
+                    <input class="form-control mb-3" type="text" name="series" id="series">
+
+                    <label for="sale_date">Data di uscita</label>
+                    <input class="form-control mb-3" type="text" name="sale_date" id="sale_date">
+
+                    <label for="type">Categoria</label>
+                    <input class="form-control mb-3" type="text" name="type" id="type">
 
                     <label for="artists">Artisti</label>
-                    <input type="text" name="artists" id="artists">
+                    <input class="form-control mb-3" type="text" name="artists" id="artists">
 
                     <label for="writers">Scrittori</label>
-                    <input type="text" name="writers" id="writers">
+                    <input class="form-control mb-5" type="text" name="writers" id="writers">
+
+                    <input class="form-control mb-3" type="submit" value="Crea">
 
                 </form>
             </div>
         </div>
+
     </div>
 
 </div>
